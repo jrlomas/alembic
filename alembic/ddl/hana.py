0758e64678a75537080e6293e8be93b4b151ccab
JR Lomas <jrlomas@cognitivecorp.io>
2018-03-04 10:05:37 -0500

    further changes to add table

--- alembic/ddl/hana.py
+++ alembic/ddl/hana.py
@@ -1,5 +1,11 @@
-from .impl import DefaultImpl
+from sqlalchemy.ext.compiler import compiles
 
+from .. import util
+from .impl import DefaultImpl
+from .base import alter_table, AddColumn, ColumnName, RenameTable,\
+    format_table_name, format_column_name, ColumnNullable, alter_column,\
+    format_server_default, ColumnDefault, format_type, ColumnType
+from sqlalchemy.sql.expression import ClauseElement, Executable
 
 class HanaImpl(DefaultImpl):
 
