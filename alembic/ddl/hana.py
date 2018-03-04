07548b5eead141f32e0f35f6977a1669980060fe
JR Lomas <jrlomas@cognitivecorp.io>
2018-03-04 10:03:59 -0500

    New changes for altering column

--- alembic/ddl/hana.py
+++ alembic/ddl/hana.py
@@ -5,3 +5,13 @@ class HanaImpl(DefaultImpl):
 
     __dialect__ = 'hana'
     transactional_ddl = True
+
+@compiles(AddColumn, 'mssql')
+def visit_add_column(element, compiler, **kw):
+    return "%s %s" % (
+        alter_table(compiler, element.table_name, element.schema),
+        hana_add_column(compiler, element.column, **kw)
+    )
+
+def mssql_add_column(compiler, column, **kw):
+    return "ADD (%s)" % compiler.get_column_specification(column, **kw)
