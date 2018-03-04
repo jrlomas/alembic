cf80f437e053fa31983825e415b09da93c3fc9de
JR Lomas <jrlomas@cognitivecorp.io>
2018-03-04 10:08:10 -0500

    Corrections

--- alembic/ddl/hana.py
+++ alembic/ddl/hana.py
@@ -19,5 +19,5 @@ def visit_add_column(element, compiler, **kw):
         hana_add_column(compiler, element.column, **kw)
     )
 
-def mssql_add_column(compiler, column, **kw):
+def hana_add_column(compiler, column, **kw):
     return "ADD (%s)" % compiler.get_column_specification(column, **kw)
